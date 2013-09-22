5ec239367a77233f71fd2fb0786eb1b9bc797f3d
dmillerw <dmillerw@gmail.com>
2013-09-21 17:51:57 -0700

    Formatting fixes

--- common/electrodynamics/world/gen/feature/FeatureLimestone.java
+++ common/electrodynamics/world/gen/feature/FeatureLimestone.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 import electrodynamics.addons.misc.EDAddonBOP;
-import electrodynamics.configuration.ConfigurationSettings;
 import electrodynamics.lib.block.Decorative;
 import electrodynamics.lib.block.BlockIDs;
 import electrodynamics.world.gen.feature.FeatureHandler.FeatureType;
@@ -21,97 +20,109 @@ import net.minecraftforge.common.ForgeDirection;
 
 public class FeatureLimestone extends FeatureBase {
 
-  private int frequency;
-  private int maxDepth;
-  private int maxSize;
+    private int frequency;
+    private int maxDepth;
+    private int maxSize;
 
-  public FeatureLimestone(String name) {
-    super(name);
-  }
-
-  public boolean exposed(World world, int x, int y, int z) {
-	  for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-		  int xMod = x + dir.offsetX;
-		  int yMod = y + dir.offsetY;
-		  int zMod = z + dir.offsetZ;
-		  
-		  if (world.isAirBlock(xMod, yMod, zMod)) return true;
-	  }
-	  
-	  return false;
-  }
-
-  @Override
-  public void generateFeature(Random random, int chunkX, int chunkZ, World world, boolean newGeneration) {
-    if(random.nextInt(frequency) != 1) return;
-    if (world.getWorldInfo().getTerrainType() == WorldType.FLAT) {
-      return;
+    public FeatureLimestone(String name) {
+        super(name);
     }
 
-    if (EDAddonBOP.bopLoaded && world.provider.dimensionId == EDAddonBOP.promisedLandDIMID) {
-		return;
-	}
-    
-    // Find exposed stone
-    int y;
-    int x = chunkX * 16 + 8;
-    int z = chunkZ * 16 + 8;
-    for(y=96; y >= 40; y--) {
-      // Todo: simplify
-      if(world.getBlockId(x, y, z) == Block.stone.blockID && exposed(world, x,y,z)) break;
+    public boolean exposed(World world, int x, int y, int z) {
+        for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+            int xMod = x + dir.offsetX;
+            int yMod = y + dir.offsetY;
+            int zMod = z + dir.offsetZ;
+
+            if (world.isAirBlock(xMod, yMod, zMod)) return true;
+        }
+
+        return false;
     }
-    if(y == 39) return;
 
-    int initialMapSize = (int)Math.floor(maxSize * 1.4);
+    @Override
+    public void generateFeature(Random random, int chunkX, int chunkZ, World world, boolean newGeneration) {
+        if (random.nextInt(frequency) != 1) return;
+        if (world.getWorldInfo().getTerrainType() == WorldType.FLAT) {
+            return;
+        }
 
-    HashMap<List<Integer>,Integer> depthMap = new HashMap<List<Integer>,Integer>(initialMapSize);
-    LinkedList<List<Integer>> work = new LinkedList<List<Integer>>();
-    HashMap<List<Integer>,Boolean> placed = new HashMap<List<Integer>,Boolean>(initialMapSize);
+        if (EDAddonBOP.bopLoaded && world.provider.dimensionId == EDAddonBOP.promisedLandDIMID) {
+            return;
+        }
 
-    List<Integer> t = new ArrayList<Integer>(Arrays.asList(new Integer(x), new Integer(y), new Integer(z)));
-    work.add(t);
-    depthMap.put(t,new Integer(0));
-    while(work.size() > 0 && placed.size() < maxSize){
-      t = work.remove();
-      Integer d = depthMap.get(t);
+        // Find exposed stone
+        int y;
+        int x = chunkX * 16 + 8;
+        int z = chunkZ * 16 + 8;
+        for (y = 96; y >= 40; y--) {
+            // Todo: simplify
+            Block block = Block.blocksList[world.getBlockId(x, y, z)];
+            if ((block != null && block.isGenMineableReplaceable(world, x, y, z, Block.stone.blockID)) && exposed(world, x, y, z)) break;
+        }
+        if (y == 39) return;
 
-      if(d > maxDepth) continue;
-      int x1 = t.get(0).intValue(); int y1 = t.get(1).intValue(); int z1 = t.get(2).intValue();
-      if(d > 0 && exposed(world, x1, y1, z1)){
-        d = 0; depthMap.put(t,new Integer(0));
-      }
+        int initialMapSize = (int) Math.floor(maxSize * 1.4);
 
-      Block block = Block.blocksList[world.getBlockId(x1, y1, z1)];
-      
-      if(block == null || block.isGenMineableReplaceable(world, x1, y1, z1, Block.stone.blockID)){
-        world.setBlock(x1, y1, z1, BlockIDs.BLOCK_DECORATIVE_ID, 0, Decorative.LIMESTONE.ordinal());
-        placed.put(t, true);
+        HashMap<List<Integer>, Integer> depthMap = new HashMap<List<Integer>, Integer>(initialMapSize);
+        LinkedList<List<Integer>> work = new LinkedList<List<Integer>>();
+        HashMap<List<Integer>, Boolean> placed = new HashMap<List<Integer>, Boolean>(initialMapSize);
 
-        d++;
-        List<Integer> u;
-        Integer x2 = t.get(0); Integer y2 = t.get(1); Integer z2 = t.get(2);
-        u = new ArrayList<Integer>(Arrays.asList(x2+1, y2, z2));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-        u = new ArrayList<Integer>(Arrays.asList(x2-1, y2, z2));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-        u = new ArrayList<Integer>(Arrays.asList(x2, y2+1, z2));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-        u = new ArrayList<Integer>(Arrays.asList(x2, y2-1, z2));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-        u = new ArrayList<Integer>(Arrays.asList(x2, y2, z2+1));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-        u = new ArrayList<Integer>(Arrays.asList(x2, y2, z2-1));
-        if(!placed.containsKey(u)) work.add(u); if(depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u,d);
-      }
+        List<Integer> t = new ArrayList<Integer>(Arrays.asList(new Integer(x), new Integer(y), new Integer(z)));
+        work.add(t);
+        depthMap.put(t, new Integer(0));
+        while (work.size() > 0 && placed.size() < maxSize) {
+            t = work.remove();
+            Integer d = depthMap.get(t);
+
+            if (d > maxDepth) continue;
+            int x1 = t.get(0).intValue();
+            int y1 = t.get(1).intValue();
+            int z1 = t.get(2).intValue();
+            if (d > 0 && exposed(world, x1, y1, z1)) {
+                d = 0;
+                depthMap.put(t, new Integer(0));
+            }
+
+            Block block = Block.blocksList[world.getBlockId(x1, y1, z1)];
+
+            if (block == null || block.isGenMineableReplaceable(world, x1, y1, z1, Block.stone.blockID)) {
+                world.setBlock(x1, y1, z1, BlockIDs.BLOCK_DECORATIVE_ID, 0, Decorative.LIMESTONE.ordinal());
+                placed.put(t, true);
+
+                d++;
+                List<Integer> u;
+                Integer x2 = t.get(0);
+                Integer y2 = t.get(1);
+                Integer z2 = t.get(2);
+                u = new ArrayList<Integer>(Arrays.asList(x2 + 1, y2, z2));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+                u = new ArrayList<Integer>(Arrays.asList(x2 - 1, y2, z2));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+                u = new ArrayList<Integer>(Arrays.asList(x2, y2 + 1, z2));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+                u = new ArrayList<Integer>(Arrays.asList(x2, y2 - 1, z2));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+                u = new ArrayList<Integer>(Arrays.asList(x2, y2, z2 + 1));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+                u = new ArrayList<Integer>(Arrays.asList(x2, y2, z2 - 1));
+                if (!placed.containsKey(u)) work.add(u);
+                if (depthMap.get(u) == null || depthMap.get(u) > d) depthMap.put(u, d);
+            }
+        }
     }
-  }
 
-  @Override
-  public void handleConfig(Configuration config) {
-    //Todo: double defaults?
-    this.frequency = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "frequency", 5).getInt(5);
-    this.maxDepth = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "max_depth", 3).getInt(3) - 1;
-    this.maxSize = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "max_size", 2048).getInt(2048);
-  }
+    @Override
+    public void handleConfig(Configuration config) {
+        //Todo: double defaults?
+        this.frequency = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "frequency", 5).getInt(5);
+        this.maxDepth = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "max_depth", 3).getInt(3) - 1;
+        this.maxSize = config.get(FeatureHandler.getFeatureCategory(FeatureType.LIMESTONE), "max_size", 4096).getInt(4096);
+    }
 
 }
